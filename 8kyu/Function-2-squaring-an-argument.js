cfac791e074ed6a3a76c23738ef3ead85b6beabf
Petro Sahal(hhgtl) <petrosahal66@gmail.com>
2025-01-27 21:12:11 +0200

    new file Function-2-squaring-an-argument in codewars/8kyu folder

---
+++ 8kyu/Function-2-squaring-an-argument.js
@@ -0,0 +1,8 @@
+// DESCRIPTION
+// Now you have to write a function that takes an argument and returns the square of it.
+// SOLUTION
+const square = (num) => {
+  return num ** 2;
+};
+
+console.log(square(3));
