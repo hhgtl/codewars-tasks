b921e3c3b44bd0186a43bd42816205cd99a71321
Petro Sahal(hhgtl) <petrosahal66@gmail.com>
2025-03-08 22:51:11 +0200

    new file get-character-from-ASCII-Value in codewars/8kyu folder

---
+++ 8kyu/get-character-from-ASCII-Value.js
@@ -0,0 +1,8 @@
+// DESCRIPTION
+// Now you have to write a function that takes an argument and returns the square of it.
+// SOLUTION
+function getChar(c) {
+  return String.fromCharCode(c);
+}
+
+console.log(getChar(55));
